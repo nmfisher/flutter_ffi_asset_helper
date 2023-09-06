5c01903a7e1108c2ad6384969bbf8a508448beb7
Nick Fisher <nick.fisher@polyvox.app>
2023-09-06 11:34:13 +0800

    return properly if path not found

--- macos/Classes/FlutterFfiAssetHelperPlugin.swift
+++ macos/Classes/FlutterFfiAssetHelperPlugin.swift
@@ -13,22 +13,23 @@ public class FlutterFfiAssetHelperPlugin: NSObject, FlutterPlugin {
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
     case "assetToByteArrayPointer":
-      print("Loading asset at asset path \(call.arguments)");
+      print("Loading asset at asset path \(call.arguments!)");
       let bundle = Bundle.init(identifier: "io.flutter.flutter.app")!
       let path = bundle.path(forResource:call.arguments as! String, ofType: nil, inDirectory: "flutter_assets")
       if(path == nil) {
-        print("File not present in bundle : \(call.arguments)")
+        print("File not present in bundle : \(call.arguments!)")
         result(nil)
-      }
-      do {
-        let data = try Data(contentsOf: URL(fileURLWithPath:path!))
-        let nsData = data as NSData
-        _assets[call.arguments as! String] = nsData
-        let ptr = Int(bitPattern:nsData.bytes)
-        let length = nsData.length
-        result([ptr, length])
-      } catch {
-        result(nil)
+      } else {
+          do {
+              let data = try Data(contentsOf: URL(fileURLWithPath:path!))
+              let nsData = data as NSData
+              _assets[call.arguments as! String] = nsData
+              let ptr = Int(bitPattern:nsData.bytes)
+              let length = nsData.length
+              result([ptr, length])
+          } catch {
+              result(nil)
+          }
       }
     case "free":
       _assets[call.arguments as! String] = nil
