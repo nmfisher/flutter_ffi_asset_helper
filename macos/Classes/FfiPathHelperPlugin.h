bcc71cbe404b72707bf144770537c601f14f9418
Nick Fisher <nick.fisher@polyvox.app>
2023-09-06 09:50:10 +0800

    support MacOS

--- macos/Classes/FfiPathHelperPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <FlutterMacOS/FlutterMacOS.h>
-
-@interface FlutterFfiAssetHelperPlugin : NSObject<FlutterPlugin>
-@end
--- macos/Classes/FlutterFfiAssetHelperPlugin.swift
+++ macos/Classes/FlutterFfiAssetHelperPlugin.swift
@@ -3,17 +3,28 @@ import FlutterMacOS
 
 public class FlutterFfiAssetHelperPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_ffi_asset_helper", binaryMessenger: registrar.messenger)
+    let channel = FlutterMethodChannel(name: "app.polyvox/flutter_ffi_asset_helper", binaryMessenger: registrar.messenger)
     let instance = FlutterFfiAssetHelperPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "getPlatformVersion":
-      result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
+    case "assetToByteArrayPointer":
+      result(FlutterMethodNotImplemented) 
+    case "getFdFromAsset":
+      print("Loading asset at asset path \(call.arguments)");
+      let bundle = Bundle.init(identifier: "io.flutter.flutter.app")!
+      let path = bundle.path(forResource:call.arguments as! String, ofType: nil, inDirectory: "flutter_assets")
+      if(path == nil) {
+        print("File not present in bundle : \(call.arguments)")
+      }
+      result(path);
+    case "closeFd":
+      // noop
+      result(true)
     default:
-      result(FlutterMethodNotImplemented)
+      result(FlutterMethodNotImplemented) 
     }
   }
 }
