735ad4e0f7bd23b4b481483f9108e7dd7e00d32a
Nick Fisher <nick.fisher@polyvox.app>
2023-09-06 09:49:59 +0800

    update example project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,6 +17,23 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Plugin example app'),
+            ),
+            body: Body()));
+  }
+}
+
+class Body extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => BodyState();
+}
+
+class BodyState extends State<Body> {
   final _FlutterFfiAssetHelperPlugin = FlutterFfiAssetHelper();
 
   FFIAsset? _asset;
@@ -29,47 +46,46 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Plugin example app'),
-      ),
-      body: Center(
-        child: Column(children: [
-          ElevatedButton(
-            child: const Text("Load"),
-            onPressed: () async {
-              // var ptr = ImmutableBuffer.fromAsset("assets/some_test_asset.txt");
-              _asset =
-                  await _FlutterFfiAssetHelperPlugin.assetToByteArrayPointer(
-                      "assets/some_test_asset.txt");
-              setState(() {
-                var ptr = Pointer<Char>.fromAddress(_asset!.data);
-                var data = List.generate(
-                    _asset!.length, (index) => ptr.elementAt(index).value);
-                _contents = Utf8Decoder().convert(data);
-              });
-            },
-          ),
-          _contents == null ? Container() : Text(_contents!),
-          ElevatedButton(
-            child: const Text("Free"),
-            onPressed: _asset == null
-                ? null
-                : () async {
-                    await _FlutterFfiAssetHelperPlugin.free(_asset!);
-                  },
-          ),
-          ElevatedButton(
-            child: const Text("Get file descriptor"),
-            onPressed: () async {
-              var path = await _FlutterFfiAssetHelperPlugin.getFdFromAsset(
-                  "assets/some_test_asset2.txt");
-              await _FlutterFfiAssetHelperPlugin.closeFd(path);
-            },
-          ),
-        ]),
-      ),
-    ));
+    return Center(
+      child: Column(children: [
+        ElevatedButton(
+          child: const Text("Load"),
+          onPressed: () async {
+            // var ptr = ImmutableBuffer.fromAsset("assets/some_test_asset.txt");
+            _asset = await _FlutterFfiAssetHelperPlugin.assetToByteArrayPointer(
+                "assets/some_test_asset.txt");
+            setState(() {
+              var ptr = Pointer<Char>.fromAddress(_asset!.data);
+              var data = List.generate(
+                  _asset!.length, (index) => ptr.elementAt(index).value);
+              _contents = Utf8Decoder().convert(data);
+            });
+          },
+        ),
+        _contents == null ? Container() : Text(_contents!),
+        ElevatedButton(
+          child: const Text("Free"),
+          onPressed: _asset == null
+              ? null
+              : () async {
+                  await _FlutterFfiAssetHelperPlugin.free(_asset!);
+                },
+        ),
+        ElevatedButton(
+          child: const Text("Get file descriptor"),
+          onPressed: () async {
+            var path = await _FlutterFfiAssetHelperPlugin.getFdFromAsset(
+                "assets/some_test_asset2.txt");
+            await showDialog(
+                builder: (ctx) {
+                  return Center(
+                      child: Container(color: Colors.white, child: Text(path)));
+                },
+                context: context);
+            await _FlutterFfiAssetHelperPlugin.closeFd(path);
+          },
+        ),
+      ]),
+    );
   }
 }
