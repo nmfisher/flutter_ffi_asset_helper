b1f673844b30e86e3a73a74c8575882345662ca4
Nick Fisher <nick.fisher@polyvox.app>
2023-09-18 16:17:00 +0800

    fix Android

--- example/android/app/src/main/kotlin/com/example/ffi_path_helper_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package app.polyvox.flutter_ffi_asset_helper_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- macos/Classes/FlutterFfiAssetHelperPlugin.swift
+++ macos/Classes/FlutterFfiAssetHelperPlugin.swift
@@ -12,41 +12,41 @@ public class FlutterFfiAssetHelperPlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "assetToByteArrayPointer":
-      print("Loading asset at asset path \(call.arguments!)");
-      let bundle = Bundle.init(identifier: "io.flutter.flutter.app")!
-      let path = bundle.path(forResource:call.arguments as! String, ofType: nil, inDirectory: "flutter_assets")
-      if(path == nil) {
-        print("File not present in bundle : \(call.arguments!)")
-        result(nil)
-      } else {
-          do {
-              let data = try Data(contentsOf: URL(fileURLWithPath:path!))
-              let nsData = data as NSData
-              _assets[call.arguments as! String] = nsData
-              let ptr = Int(bitPattern:nsData.bytes)
-              let length = nsData.length
-              result([ptr, length])
-          } catch {
-              result(nil)
-          }
-      }
-    case "free":
-      _assets[call.arguments as! String] = nil
-      result(true)
-    case "getFdFromAsset":
-      print("Loading asset at asset path \(call.arguments)");
-      let bundle = Bundle.init(identifier: "io.flutter.flutter.app")!
-      let path = bundle.path(forResource:call.arguments as! String, ofType: nil, inDirectory: "flutter_assets")
-      if(path == nil) {
-        print("File not present in bundle : \(call.arguments)")
-      }
-      result(path);
-    case "closeFd":
-      // noop
-      result(true)
-    default:
-      result(FlutterMethodNotImplemented) 
+      case "assetToByteArrayPointer":
+        print("Loading asset at asset path \(call.arguments!)");
+        let bundle = Bundle.init(identifier: "io.flutter.flutter.app")!
+        let path = bundle.path(forResource:call.arguments as! String, ofType: nil, inDirectory: "flutter_assets")
+        if(path == nil) {
+          print("File not present in bundle : \(call.arguments!)")
+          result(nil)
+        } else {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath:path!))
+                let nsData = data as NSData
+                _assets[call.arguments as! String] = nsData
+                let ptr = Int(bitPattern:nsData.bytes)
+                let length = nsData.length
+                result([ptr, length])
+            } catch {
+                result(nil)
+            }
+        }
+      case "free":
+        _assets[call.arguments as! String] = nil
+        result(true)
+      case "getFdFromAsset":
+        print("Loading asset at asset path \(call.arguments)");
+        let bundle = Bundle.init(identifier: "io.flutter.flutter.app")!
+        let path = bundle.path(forResource:call.arguments as! String, ofType: nil, inDirectory: "flutter_assets")
+        if(path == nil) {
+          print("File not present in bundle : \(call.arguments)")
+        }
+        result(path);
+      case "closeFd":
+        // noop
+        result(true)
+      default:
+        result(FlutterMethodNotImplemented) 
     }
   }
 }
